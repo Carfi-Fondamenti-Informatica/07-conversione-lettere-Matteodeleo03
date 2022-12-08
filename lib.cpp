06b7b0ef6d4c158d162fc63bcefb97522b38bf70
Matteodeleo03 <117951922+Matteodeleo03@users.noreply.github.com>
2022-12-09 00:05:36 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
-
+#include "lib.h"
+bool convert (char & a) {
+    if ((a>=65 && a <= 90) || (a>=97 && a<=122)) {
+        if (a>=65 && a<=90) {
+            a+=32;
+        } else {
+            a-=32; 
+        } return 1;
+    } else {
+        return 0;
+    }
+}
